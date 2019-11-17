08973d5d72551772c436daf8ca5a358574feb437
kirillTarasov <tarasovk2001@yandex.ru>
2019-11-17 23:06:28 +0500

    client-state fix

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,5 +1,6 @@
 let editor = document.getElementById('editor');
-if (!editor.value) editor.value = localStorage.editorValue
+if (!localStorage.editorValue) localStorage.editorValue = '';
+editor.value = localStorage.editorValue
 editor.addEventListener('input', () => {
     localStorage.editorValue = editor.value;
 })
